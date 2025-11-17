c89dc40856bd229a762d802494b4613e273cb38f
이지성 <dlwltjd0608@gmail.com>
2025-11-17 15:32:38 +0900

    9번 이지성 코드 추가

--- src/movie/Reservation.java
+++ src/movie/Reservation.java
@@ -2,7 +2,7 @@ package movie;
 
 public class Reservation {
 
-    public boolean reserve11(Movie movie, int count) {
+    public boolean reserve9(Movie movie, int count) {
         if (movie.reserveSeat(count)) {
             System.out.println("✅ 예매 성공: " + movie.getTitle() + ", 수량: " + count);
             System.out.println("서연경");
@@ -14,6 +14,9 @@ public class Reservation {
     }
 
     public void reserve1842() {
-        System.out.println("예매 완료!_이민정");
+        System.out.println("예매 완료!_이지성");
+    }
+
+    public void reserve(Movie selectedMovie, int i) {
     }
 }
